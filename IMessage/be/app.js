4aa2781a8635d393813713eb3dd46a380c8332c7
tnlou <tnlou@justhealth.cn>
2021-03-09 10:39:16 +0800

    push

--- IMessage/be/app.js
+++ IMessage/be/app.js
@@ -29,8 +29,7 @@ app.get('/socket', (req,res) => {
 
 
 io.on('connection', socket => {
-  console.log(socket.handshake.auth.token)
-  console.log(socket.handshake.query.reciever)
+  
 
   socket.emit('message', 'welcome to Lokep`s')
 
@@ -45,6 +44,32 @@ io.on('connection', socket => {
       //给当前用户发送
       //socket.emit('setId', { id: '123456' })
   })
+
+  /**
+  * 当用户连接上时，获取生成的socket对象，并且获取对象的auth或query字段，讲对应字段存入数据库中
+  * console.log(socket.handshake.auth)
+  * console.log(socket.handshake.query)
+  */
+
+
+  /**
+  * 罗列socket监听的事件
+  * socket.on('eventName', handler)
+  * 
+  * 可以拆分文件进行管理，例如 onHandler(socket)
+  */
+
+
+
+  /**
+  * 包括对应需要向客户端发送的事件
+  * 1. 聊天（此处最好可以区分私聊，群聊，并且解析发送的消息内容，例如发送的内容中包含图片或视频，应在客户端先做上传操作）
+  * 2. 通过服务向客户端发送消息，例如点赞，评论等
+  * 
+  * 可以拆分文件进行管理，例如 onEmit(socket)
+  */
+  
+
 })
 
 server.listen(PORT, () => console.log('server is running at ' + PORT))
