84ed0d44e37459e791a6b7a58240196c87724aa3
tnlou <tnlou@justhealth.cn>
2021-03-10 18:49:16 +0800

    修改为hash

--- IMessage/fe/src/router/index.ts
+++ IMessage/fe/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, /** createWebHistory, */ RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -35,7 +35,7 @@ const routes: Array<RouteRecordRaw> = [
 
 
 const router = createRouter({
-  history: createWebHistory('/message/'),
+  history: createWebHashHistory('/message/'), //createWebHistory('/message/')
   routes
 })
 
